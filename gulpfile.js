16a4d3dbfc91b8bba2a76f8b94e1db0b03cb8054
Matthew Spence <matthew.spence@clock-work.co.uk>
2017-10-03 09:21:22 +0100

    Added licenses to project. Renamed files. Updated gulpfile to include License.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var gulp = require("gulp"),
 minify = require("gulp-minify"),
 concat = require("gulp-concat");
 
+gulp.task("licence",function(){
+	return gulp.src("LICENCE")
+		.pipe(gulp.dest("dist"));
+})
+
 gulp.task("minify",function(){
 	return gulp.src("src/**.js")
 		.pipe(concat("jquery.jscroll.js"))
@@ -13,6 +18,11 @@ gulp.task("minify",function(){
 		}))
 		.pipe(gulp.dest("dist"));
 })
-gulp.task("default",["minify"],function(){
+gulp.task("default",["minify","licence"],function(){
+	console.log("#################################################");
+	console.log("\nGulp Tasks: licence,minify");
+	console.log("\n\nWatching JS Files...\n");
+	console.log("\nCTRL+C to quit.\n");
+	console.log("#################################################");
 	gulp.watch("src/**.js",["minify"]);
 });
